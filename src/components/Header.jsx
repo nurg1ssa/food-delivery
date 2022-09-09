7f2c84cdb34a389d30a60f509227168dffc7f9dd
nurg1ssa <nurike2001@mail.ru>
2022-09-09 19:37:41 +0600

    createItem container setted

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -85,7 +85,8 @@ const Header = () => {
                                         user && user.email === 'dj.nurc00l@gmail.com' && (
                                             <Link to={'/createItem'}>
                                                 <p
-                                                    className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transiition-all duration-100 ease-in-out text-textColor text-base'>
+                                                    className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transiition-all duration-100 ease-in-out text-textColor text-base'
+                                                    onClick={()=> setIsMenu(false)}>
                                                     New Item <MdAdd /></p>
 
                                             </Link>
@@ -132,16 +133,17 @@ const Header = () => {
                                     user && user.email === 'dj.nurc00l@gmail.com' && (
                                         <Link to={'/createItem'}>
                                             <p
-                                                className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transiition-all duration-100 ease-in-out text-textColor text-base'>
+                                                className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transiition-all duration-100 ease-in-out text-textColor text-base'
+                                                onClick={() => setIsMenu(false)}>
                                                 New Item <MdAdd /></p>
 
                                         </Link>
                                     )}
                                 <ul className='flex flex-col'>
-                                    <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'>Home</li>
-                                    <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'>Menu</li>
-                                    <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'>About Us</li>
-                                    <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'>Service</li>
+                                    <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2' onClick={() => setIsMenu(false)}>Home</li>
+                                    <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2' onClick={() => setIsMenu(false)}>Menu</li>
+                                    <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2' onClick={() => setIsMenu(false)}>About Us</li>
+                                    <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2' onClick={() => setIsMenu(false)}>Service</li>
                                 </ul>
                                 <p
                                     className='m-2 p-2 rounded-md shadow-md flex items-center justify-center bg-gray-200 gap-3 cursor-pointer hover:bg-gray-300 transiition-all duration-100 ease-in-out text-textColor text-base'
--- src/utils/data.js
+++ src/utils/data.js
@@ -9,3 +9,12 @@ export const heroData = [
     {id: 3, name: 'Meals', desc: 'Mixed Kebab Plate', price: '20.25', imgSrc: C3},
     {id: 4, name: 'Fish Meals', desc: 'Fried Fish', price: '5.25', imgSrc: Fi1},
 ]
+export const categories = [
+    {id: 1, name: 'Chicken', urlParamName:'chicken'},
+    {id: 2, name: 'Curry', urlParamName:'curry'},
+    {id: 3, name: 'Rice', urlParamName:'rice'},
+    {id: 4, name: 'Fish', urlParamName:'fish'},
+    {id: 5, name: 'Fruits', urlParamName:'fruits'},
+    {id: 6, name: 'Icecreams', urlParamName:'icecreams'},
+     {id: 7, name: 'Soft Drinks', urlParamName:'drinks'},
+]
